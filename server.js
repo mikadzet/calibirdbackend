3ef1936957ee4b1912934f956ae81d519910e547
nikamikadze123 <nikamikadze123@gmail.com>
2024-11-21 18:34:28 +0400

    removed limit from leaderboard

--- server.js
+++ server.js
@@ -9,12 +9,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 
-app.use(
-  cors({
-    origin: 'https://calibird.netlify.app', // Replace with your Netlify frontend domain
-  })
-)
-app.use(bodyParser.json()) // Add this to parse JSON payloads
+app.use(cors())
+app.use(bodyParser.json())
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -37,7 +33,6 @@ app.get('/leaderboard', async (req, res) => {
     const leaderboard = await leaderboardCollection
       .find()
       .sort({ highscore: -1 })
-      .limit(10)
     res.json(leaderboard)
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch leaderboard' })
